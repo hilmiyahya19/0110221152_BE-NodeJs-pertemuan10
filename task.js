9075c0cfc50af1660335399e82b42a1ec1551fd2
hilmiyahya19 <91936796+hilmiyahya19@users.noreply.github.com>
2022-12-11 18:42:59 +0700

    Add files via upload

---
+++ task.js
@@ -0,0 +1,36 @@
+/**
+ * Fungsi untuk menampilkan hasil download
+ * @param {string} result - Nama file yang didownload
+ */
+const showDownload = (result) => {
+  console.log("Download selesai");
+  console.log(`Hasil download: ${result}`);
+};
+
+/**
+ * Fungsi untuk download file
+ * @param {function} callback - Function callback show
+ */
+
+const download = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const result = "windows-10.exe";
+      resolve(result);
+    }, 3000);
+  });
+};
+
+const main = async () => {
+  const callShowDownload = await download();
+  showDownload(callShowDownload);
+};
+
+main();
+
+/**
+ * TODO:
+ * - Refactor callback ke Promise atau Async Await
+ * - Refactor function ke ES6 Arrow Function
+ * - Refactor string ke ES6 Template Literals
+ */
